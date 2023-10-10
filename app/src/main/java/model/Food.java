7441f99285b265cb85b951656c6def2351e4eee9
ppitsianis <paris.pitisianis@gmail.com>
2023-10-10 14:00:16 +0300

    wrong folder

---
+++ app/src/main/java/model/Food.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.io.Serializable;
+
+public class Food implements Serializable {
+    private String foodName;
+    private int calories;
+    private int foodId;
+    private String recordDate;
+}
